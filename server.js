08414a8c343b405b2f53e8aa0f78a512f8214c62
Ruben Alvarado <ruben.alvarado0@walmart.com>
2023-04-12 18:29:24 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const IP = require('ip');
+
+app.get('/', (req, res) => {
+    const ipAddress = IP.address();
+    res.send(ipAddress)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
