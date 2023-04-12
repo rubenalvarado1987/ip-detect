d9a59f44fe9b3390bed5fbc4cdee0f3b159a89c0
Ruben Alvarado <ruben.alvarado0@walmart.com>
2023-04-12 19:18:35 -0400

    test7

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const app = express()
 const port = 80
 const IP = require('ip');
 
+
+app.get('/hola', (req, res) => {
+    res.send("HOLA")
+    console.log(`HOLA`);
+});
+
 app.get('/address', (req, res) => {
     const ipAddress = IP.address();
     res.send("IP:"+ipAddress)
 
     console.log(`IP`,IP);
     console.log(`ipAddress`,ipAddress);
-})
+});
 
 app.get('/ip',function(req, response) {
 
@@ -17,7 +23,7 @@ app.get('/ip',function(req, response) {
     const ipAddress = req.header('x-forwarded-for') || req.socket.remoteAddress;
     res.send("ipAddress:"+ipAddress);
     
-  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on ports ${port}`)
