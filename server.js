c0d24a4b4f62a5324888234767b1ce8f3771c71e
Ruben Alvarado <ruben.alvarado0@walmart.com>
2023-04-12 19:20:07 -0400

    test8

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 80
-const IP = require('ip');
+//const IP = require('ip');
 
 
 app.get('/hola', (req, res) => {
@@ -9,21 +9,21 @@ app.get('/hola', (req, res) => {
     console.log(`HOLA`);
 });
 
-app.get('/address', (req, res) => {
-    const ipAddress = IP.address();
-    res.send("IP:"+ipAddress)
+// app.get('/address', (req, res) => {
+//     const ipAddress = IP.address();
+//     res.send("IP:"+ipAddress)
 
-    console.log(`IP`,IP);
-    console.log(`ipAddress`,ipAddress);
-});
+//     console.log(`IP`,IP);
+//     console.log(`ipAddress`,ipAddress);
+// });
 
-app.get('/ip',function(req, response) {
+// app.get('/ip',function(req, response) {
 
-    console.log("IP...");
-    const ipAddress = req.header('x-forwarded-for') || req.socket.remoteAddress;
-    res.send("ipAddress:"+ipAddress);
+//     console.log("IP...");
+//     const ipAddress = req.header('x-forwarded-for') || req.socket.remoteAddress;
+//     res.send("ipAddress:"+ipAddress);
     
-});
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on ports ${port}`)
